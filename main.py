75c3b57d168f8c79456e4ea6e60269d65d4a1a85
Arty318 <artemik123z@gmail.com>
2025-09-19 20:17:11 +0500

    FastAPI Sqrt-Calc

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from fastapi.templating import Jinja2Templates
 from math import atan,cos, sin, pi
 
 def is_complex(user_data: str) -> bool | tuple:
+    '''Функция проверяет, является ли полученное число формой записи комплексного числа вида a+bi'''
     if user_data.count('i')==1 and user_data[-1]=='i':
         user_data = user_data[:-1]
-        if user_data.count('+')==1 and user_data.count('-')==0 and user_data.find('+')!=0 and user_data.find('+')!=(len(user_data)-1):
+        if user_data.count('+')==1 and user_data.count('-')==0 and user_data.find('+')!=0 and user_data.find('+')!=(len(user_data)-1): # если число имеет вид a+bi
             user_data = user_data.split('+')
             if user_data[0].isdigit() and user_data[1].isdigit():
                 return True
-        elif user_data.count('+')==0 and user_data.count('-')==1 and user_data.find('-')!=0 and user_data.find('-')!=(len(user_data)-1):
+        elif user_data.count('+')==0 and user_data.count('-')==1 and user_data.find('-')!=0 and user_data.find('-')!=(len(user_data)-1): # если число имеет вид a-bi
             user_data = user_data.split('-')
             if user_data[0].isdigit() and user_data[1].isdigit():
                 return True
     return False
 
 def get_re_and_im(user_data: str) -> tuple:
+    '''Функция, извлекающая действительную и мнимую часть из строкового представления комплексного числа'''
     user_data = user_data[:-1]
     if user_data.count('+')==1:
         user_data = user_data.replace('+', ' ')
@@ -26,29 +28,16 @@ def get_re_and_im(user_data: str) -> tuple:
     return int(user_data[0]),int(user_data[1])
 
 def get_complex_sqrt(re: int, im: int):
-    r = (re**2 + im**2)**0.5
-    phi = atan(im / re)
+    '''Функция, извлекающая квадратный корень из комплексного числа с ненулевыми a и b'''
+    D = (-4*re + 4*((re**2 + im**2)**0.5))/8
 
+    sqrt_im_1 = D**0.5
+    sqrt_im_2 = -D**0.5
 
-    result_re_1 = str((r**0.5)*(cos(phi/2)))
-    result_im_1 = str((r**0.5)*(sin(phi/2)))
+    sqrt_re_1 = re/(2*sqrt_im_1)
+    sqrt_re_2 = re/(2*sqrt_im_2)
 
-    result_re_2 = str((r**0.5)*(cos((phi+2*pi)/2)))
-    result_im_2 = str((r**0.5)*(sin((phi+2*pi)/2)))
-
-    if result_im_1[0] == '-':
-        result_complex_1 = "Первый корень = " + result_re_1 + result_im_1 + 'i'
-    else:
-        result_complex_1 = "Первый корень = " + result_re_1 + '+' + result_im_1 + 'i'
-
-    if result_im_2[0] == '-':
-        result_complex_2 = "Второй корень = " + result_re_2 + result_im_2 + 'i'
-    else:
-        result_complex_2 = "Второй корень = " + result_re_2 + '+' + result_im_2 + 'i'
-
-    return result_complex_1, result_complex_2
-
-
+    return f"{sqrt_re_1}+{sqrt_im_1}i",f"{sqrt_re_2}-{sqrt_im_2}i"
 
 
 
@@ -62,13 +51,21 @@ def get_main_page(request: Request):
 
 @app.post("/post_num")
 def get_number(request: Request, number: str = Form(...)):
-    if is_complex(number):
-        re_and_im = get_re_and_im(number)
-        print("Квадратный корень из вашего комплексного числа:" , get_complex_sqrt(*re_and_im))
-        return templates.TemplateResponse("index.html", {"request": request, "number": number, "result": get_complex_sqrt(*re_and_im), "type_of_result": "Квадратный корень из вашего комплексного числа"})
-    else:
-        arifm_sqrt = int(number)**0.5
-        print("Квадратный корень из вашего числа:", arifm_sqrt)
-        return templates.TemplateResponse("index.html", {"request": request, "number": number, "result": arifm_sqrt, "type_of_result": "Квадратный корень из вашего числа"})
-
-
+    if number.isdigit() and int(number)>0:
+        return templates.TemplateResponse("index.html", {"request": request, "type_of_result": "Результат вычисления корня из вашего целого, положительного числа:", "number" : number, "result": f"Первый корень равен: {(int(number))**0.5} ; Второй корень равен: {-(int(number)**0.5)}"})
+    if number.count(".")==1 and float(number)>0:
+        buf = number.split(".")
+        if buf[0].isdigit() and buf[1].isdigit():
+            return templates.TemplateResponse("index.html", {"request": request, "type_of_result": "Результат вычисления корня из вашего рационального, положительного числа:", "number": number, "result": f"Первый корень равен: {(float(number))**0.5} ; Второй корень равен: {-(float(number))**0.5}"})
+    elif number.isdigit() and int(number)==0:
+        return templates.TemplateResponse("index.html", {"request": request, "type_of_result": "Результат вычисления корня из нуля:", "number": number, "result": f"Корень равен: {0}"})
+    elif number.isdigit() and int(number)<0:
+        return templates.TemplateResponse("index.html", {"request": request, "type_of_result": "Результат вычисления корня из вашего целого, отрицательного числа:", "number": number, "result": f"Первый корень равен: {(-1*int(number))**0.5}i ; Второй корень равен: {-((-1*int(number))**0.5)}i"})
+    elif number.count(".")==1 and float(number)<0:
+        buf = number.split(".")
+        if buf[0].isdigit() and buf[1].isdigit():
+            return templates.TemplateResponse("index.html", {"request": request, "type_of_result": "Результат вычисления корня из вашего рационального, отрицательного числа:", "number": number, "result": f"Первый корень равен: {(-1*float(number))**0.5}i ; Второй корень равен: {-((-1*float(number))**0.5)}i"})
+    elif is_complex(number):
+        a,b = get_re_and_im(number)
+        result = get_complex_sqrt(a,b)
+        return templates.TemplateResponse("index.html", {"request": request, "type_of_result": "Результат вычисления корня из вашего комплексного числа:", "number": number, "result": f"Первый корень равен: {result[0]} ; Второй корень равен: {result[1]}"})
